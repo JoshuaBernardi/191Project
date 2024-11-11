600eee2b4f48f789c897605784b91a36dd3d6037
botlife123 <zalmafarah2@gmail.com>
2024-11-10 20:41:41 -0800

    Update SearchBookScreen.java

--- gui/SearchBookScreen.java
+++ gui/SearchBookScreen.java
@@ -1,6 +1,11 @@
 package gui;
 
-public class SearchBookScreen
-{
+import java.awt.*;
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+public class SearchBookScreen extends JFrame
+{
+	
 }
