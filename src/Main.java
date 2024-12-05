60ebab65e244f026b6051366b8e300f2ef47c297
JoshuaBernardi <87204545+JoshuaBernardi@users.noreply.github.com>
2024-12-04 16:06:37 -0800

    fixed build

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+
+import gui.LoginScreen;
+
+/**
+ * class as the starting point for the application
+ */
+public class Main
+{
+
+	public static void main(String[] args)
+	{
+		LoginScreen loginScreen = new LoginScreen();
+		loginScreen.setVisible(true);
+		
+	}
+
+}
