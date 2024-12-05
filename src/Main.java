fe1dbb31d5c9a7499d93b6b74e9b11f92b9d6cdd
JoshuaBernardi <87204545+JoshuaBernardi@users.noreply.github.com>
2024-12-04 20:03:00 -0800

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -7,7 +7,7 @@ import gui.LoginScreen;
  * class as the starting point for the application
  * 
  * This program is the GUI that manages the library
- * 
+ * git hub test
  * Change
  */
 public class Main
